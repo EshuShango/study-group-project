e35a42d26dce71a55c1df6c3448c673a3c806562
EshuShango <tobiakinyemi21@gmail.com>
2023-02-14 10:15:44 -0600

    added validateEmail algo

--- models/User.js
+++ models/User.js
@@ -1,7 +1,24 @@
 //! WE HAVE TO FILL IN NECESSARY CODE etc
-const {Model, DataTypes} = require("sequelize");
-const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+//* below we create a async function to validate that a real email is put within
+//* the Email input etc
+const validateEmail = async (email) => {
+  typeof email !== 'string' ? console.error('Email must be a string') : null;
+
+  let atpos = email.indexOf('@');
+  let dotpos = email.lastIndexOf('.com');
+
+  return atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length
+    ? false
+    : true;
+};
+const email = '';
+const result = validateEmail(email);
+console.log(result);
+//* ---DON'T HAVE TO USE just thought it be cool + experimenting etc ---
 
 class User extends Model {
   checkPassword(loginPw) {
@@ -26,23 +43,25 @@ User.init(
       allowNull: false,
     },
     username: {
-      type:sequelize.STRING,
+      type: sequelize.STRING,
       allowNull: false,
       unique: true,
       validate: {
         //validating email
         isEmail: true,
-      }
+        //may not work
+        validate: validateEmail,
+      },
     },
     password: {
-      type:sequelize.STRING,
+      type: sequelize.STRING,
       allowNull: false,
       validate: {
-        //validate password length here, and any other requirements we 
+        //validate password length here, and any other requirements we
         //and users to have for passwords
-        len:[8]
-      }
-    }
+        len: [8],
+      },
+    },
   },
   {
     hooks: {
@@ -51,15 +70,18 @@ User.init(
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
         return updatedUserData;
-      }
+      },
     },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "user",
+    modelName: 'user',
   }
 );
 
