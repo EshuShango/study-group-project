6a838e2ba1da7f3f745444b0a443cb0fad0a356e
Kristen Schaefer <kristencschaefer@gmail.com>
2023-02-16 09:17:50 -0600

    added group name and abilty to serve resources by group number

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,41 +11,45 @@ router.get('/login', async (req, res) => {
 
 router.get('/resources/:id', async (req, res) => {
   try {
-      const resourceData = await Resource.findAll({
+    const resourceData = await Resource.findAll({
+      include: [
+        {
+          model: User,
+          required: false
+        }
+      ]
+    });
+    console.log("------------anything---------------")
+    const groupData = await Group.findByPk(req.params.id,
+      {
+        where:
+        {
+          resource_id: req.params.id
+        },
         include: [
           {
             model: User,
+            as: "users_groups",
             required: false
           }
         ]
       });
-      console.log("------------anything---------------")
-      const groupData = await Group.findByPk(req.params.id,
-        {
-          include: [
-            {
-              model: User,
-              as: "users_groups",
-              required: false
-            }
-          ]
-        });
-      console.log("------------more anythings---------------")
-      const bruh = resourceData.map((data) => data.get({ plain: true }));
-      const group = groupData.get({ plain: true });
-      res.render('resources', { bruh, group });
+    console.log("------------more anythings---------------")
+    const bruh = resourceData.map((data) => data.get({ plain: true }));
+    const group = groupData.get({ plain: true });
+    res.render('resources', { bruh, group });
   }
   catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
 
 router.get("/videoroom", async (req, res) => {
   try {
-      res.render("videochat");
+    res.render("videochat");
   }
   catch (err) {
-      res.status(500).json(err);
+    res.status(500).json(err);
   }
 })
 
