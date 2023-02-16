de11e6eee3ff8e2c3c1798540c3af1229200b231
EshuShango <tobiakinyemi21@gmail.com>
2023-02-16 10:12:54 -0600

    worked on js for chat, need asst

--- public/js/login.js
+++ public/js/login.js
@@ -2,48 +2,30 @@ const loginFormBtn = document.getElementById('loginFormBtn');
 const usernameField = document.getElementById('loginName');
 const loginPasswordField = document.getElementById('loginPassword');
 
-
 async function login(e) {
-    console.log(e.target);
-    e.stopPropagation();
-    e.preventDefault();
+  console.log(e.target);
+  e.stopPropagation();
+  e.preventDefault();
 
-    const obj = {
-        "username": usernameField.value,
-        "password": loginPasswordField.value
-    };
+  const obj = {
+    username: usernameField.value,
+    password: loginPasswordField.value,
+  };
 
-    const response = await fetch('/api/user/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(obj)
-    });
+  const response = await fetch('/api/user/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+  });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        usernameField.setCustomValidity('Invalid username or password');
-    }
-};
-
-loginFormBtn.addEventListener('click', login);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    usernameField.setCustomValidity('Invalid username or password');
+  }
+}
 
 loginFormBtn.addEventListener('click', login);
+
+//? do we need two or is this a typo below?
+// loginFormBtn.addEventListener('click', login);
