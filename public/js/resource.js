d7a288f809165b6820e3f985e63a6c58f4469703
Kristen Schaefer <kristencschaefer@gmail.com>
2023-02-16 10:30:40 -0600

    trying to add js to resources page

--- public/js/resource.js
+++ public/js/resource.js
@@ -1,1 +1,31 @@
-// use this to grab from user input and add to database
+const resourceFormBtn = document.getElementById('addResource');
+const urlField = document.getElementById('formurl');
+const descriptionField = document.getElementById('formdescription');
+const userField = req.body.user_id;
+const groupField = req.body.group_id;
+
+async function addResource() {
+  const obj = {
+      "url": urlField.value,
+      "description": descriptionField.value,
+      "user_id": userField.value,
+      "group_id": groupField.value,
+  };
+  const response = await fetch('/api/resource/', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(obj)
+});
+
+if (response.ok) {
+    document.location.replace('/');
+} else {
+    alert(response.statusText);
+}
+};
+console.log(addResource);
+
+resourceFormBtn.addEventListener('click', addResource);
+
